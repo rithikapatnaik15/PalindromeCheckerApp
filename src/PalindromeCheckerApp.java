317ffd5fcc322f158643b9a0decd6d16eae28eda
rithikapatnaik15 <rithikapatnaik15122006@gmail.com>
2026-03-09 15:56:10 +0530

    rithika

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,34 @@
 public class PalindromeCheckerApp {
+}import java.util.Scanner;
+
+public class PalindromeIgnoreCaseSpace {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        str = str.toLowerCase();
+        str = str.replaceAll("\\s", "");
+
+        int start = 0;
+        int end = str.length() - 1;
+        boolean isPalindrome = true;
+
+        while(start < end) {
+            if(str.charAt(start) != str.charAt(end)) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        if(isPalindrome)
+            System.out.println("The string is a Palindrome");
+        else
+            System.out.println("The string is NOT a Palindrome");
+    }
 }
