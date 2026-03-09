1f9b23da263236ef5e6ee62a682bce72758d1386
rithikapatnaik15 <rithikapatnaik15122006@gmail.com>
2026-03-09 15:24:56 +0530

    rithika

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,35 @@
 public class PalindromeCheckerApp {
+}import java.util.*;
+
+public class PalindromeQueueStack {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        Queue<Character> queue = new LinkedList<>();
+        Stack<Character> stack = new Stack<>();
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        for(int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            queue.add(ch);      // enqueue
+            stack.push(ch);     // push
+        }
+
+        boolean isPalindrome = true;
+
+        while(!queue.isEmpty()) {
+            if(queue.remove() != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if(isPalindrome)
+            System.out.println("The string is a Palindrome");
+        else
+            System.out.println("The string is NOT a Palindrome");
+    }
 }
