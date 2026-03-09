5d9432f4e43e8a815894bd67efa9ebe0d1fe559f
rithikapatnaik15 <rithikapatnaik15122006@gmail.com>
2026-03-09 15:51:44 +0530

    rithika

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,79 @@
 public class PalindromeCheckerApp {
+}import java.util.*;
+
+class Node {
+    char data;
+    Node next;
+
+    Node(char data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class PalindromeLinkedList {
+
+    static Node reverse(Node head) {
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+
+        while(current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+
+    static boolean isPalindrome(Node head) {
+
+        Node slow = head;
+        Node fast = head;
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        Node secondHalf = reverse(slow);
+        Node firstHalf = head;
+
+        while(secondHalf != null) {
+            if(firstHalf.data != secondHalf.data)
+                return false;
+
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        Node head = null, temp = null;
+
+        for(char c : str.toCharArray()) {
+            Node newNode = new Node(c);
+
+            if(head == null) {
+                head = newNode;
+                temp = head;
+            } else {
+                temp.next = newNode;
+                temp = temp.next;
+            }
+        }
+
+        if(isPalindrome(head))
+            System.out.println("The string is a Palindrome");
+        else
+            System.out.println("The string is NOT a Palindrome");
+    }
 }
