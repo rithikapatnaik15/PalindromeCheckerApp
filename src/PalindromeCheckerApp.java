d16c5793e3a8a458727af2de84617f95dc86e0d0
rithikapatnaik15 <rithikapatnaik15122006@gmail.com>
2026-03-09 15:46:58 +0530

    rithika

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,33 @@
-public class PalindromeCheckerApp {
-}
+public class PalindromeCheckerApp {import java.util.*;
+
+    public class PalindromeDeque {
+        public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+            Deque<Character> deque = new ArrayDeque<>();
+
+            System.out.print("Enter a string: ");
+            String str = sc.nextLine();
+
+            for(int i = 0; i < str.length(); i++) {
+                deque.addLast(str.charAt(i));
+            }
+
+            boolean isPalindrome = true;
+
+            while(deque.size() > 1) {
+                char front = deque.removeFirst();
+                char rear = deque.removeLast();
+
+                if(front != rear) {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+
+            if(isPalindrome)
+                System.out.println("The string is a Palindrome");
+            else
+                System.out.println("The string is NOT a Palindrome");
+        }
+    }
